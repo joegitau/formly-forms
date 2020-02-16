a52e7f5b3d5f7174d222f06a02d502517c6f19f7
Joe <Joe@Joe-Air.local>
2020-02-16 14:28:45 +0200

    add extra form fields

--- src/app/user.model.ts
+++ src/app/user.model.ts
@@ -13,66 +13,52 @@ export class User {
   formFields() {
     return [
       {
-        className: "row",
-        fieldGroup: [
-          {
-            className: "col-xs-4",
-            type: "input",
-            key: "firstName",
-            templateOptions: {
-              label: "First Name",
-              required: true,
-              minLength: 3
-            }
-            // (alternative validation solution)
-            // ,
-            // validation: {
-            //   messages: { minLength: "Requires a minimum of 3 characters." }
-            // }
-          },
-          {
-            className: "col-xs-12",
-            type: "input",
-            key: "lastName",
-            templateOptions: {
-              label: "Last Name",
-              required: true,
-              minLength: 3
-            },
-            expressionProperties: {
-              "templateOptions.disabled": "!model.firstName"
-            }
-          }
-        ]
+        type: "input",
+        key: "firstName",
+        templateOptions: {
+          label: "First Name",
+          required: true,
+          minLength: 3
+        }
+        // (alternative validation solution)
+        // ,
+        // validation: {
+        //   messages: { minLength: "Requires a minimum of 3 characters." }
+        // }
       },
       {
-        className: "row",
-        fieldGroup: [
-          {
-            className: "col-md-6",
-            key: "email",
-            type: "input",
-            templateOptions: {
-              type: "email",
-              placeholder: "Email",
-              label: "Email",
-              required: true,
-              email: true
-            }
-          },
-          {
-            className: "col-md-6",
-            key: "confirmEmail",
-            type: "input",
-            templateOptions: {
-              type: "email",
-              placeholder: "Verify email",
-              label: "Confirm Email",
-              required: true,
-              email: true
-            }
-          }
-        ]
+        type: "input",
+        key: "lastName",
+        templateOptions: {
+          label: "Last Name",
+          required: true,
+          minLength: 3
+        },
+        expressionProperties: {
+          "templateOptions.disabled": "!model.firstName"
+        }
+      },
+      {
+        key: "email",
+        type: "input",
+        templateOptions: {
+          type: "email",
+          placeholder: "Email",
+          label: "Email",
+          required: true,
+          email: true
+        }
+      },
+      {
+        key: "confirmEmail",
+        type: "input",
+        templateOptions: {
+          type: "email",
+          placeholder: "Verify email",
+          label: "Confirm Email",
+          required: true,
+          email: true
+        }
       },
       {
         key: "phone",
@@ -82,6 +68,32 @@ export class User {
           placeholder: "Telephone",
           label: "Telephone",
           required: false
+        }
+      },
+      {
+        key: "jobTitle",
+        type: "select",
+        defaultValue: "Frontend Developer",
+        templateOptions: {
+          label: "Select Job Title",
+          options: [
+            { label: "Choose job title", value: "" },
+            { label: "Frontend Developer", value: "frontend-developer" },
+            { label: "Backend Developer", value: "backend-developer" },
+            { label: "fullstack Developer", value: "fullstack-developer" }
+          ]
+        }
+      },
+      {
+        key: "gender",
+        type: "radio",
+        templateOptions: {
+          type: "radio",
+          label: "Gender",
+          options: [
+            { label: "Male", value: "male" },
+            { label: "Female", value: "female" }
+          ]
         }
       },
       {
@@ -95,29 +107,23 @@ export class User {
         }
       },
       {
-        className: "row",
-        fieldGroup: [
-          {
-            className: "col-md-6",
-            key: "postalCode",
-            type: "input",
-            templateOptions: {
-              type: "text",
-              label: "Postal Code",
-              required: true
-            }
-          },
-          {
-            key: "city",
-            type: "input",
-            templateOptions: {
-              type: "text",
-              placeholder: "City",
-              label: "City",
-              required: true
-            }
-          }
-        ]
+        key: "postalCode",
+        type: "input",
+        templateOptions: {
+          type: "text",
+          label: "Postal Code",
+          required: true
+        }
+      },
+      {
+        key: "city",
+        type: "input",
+        templateOptions: {
+          type: "text",
+          placeholder: "City",
+          label: "City",
+          required: true
+        }
       }
     ] as FormlyFieldConfig[];
   }
